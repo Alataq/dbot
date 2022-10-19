c4009e33a80164ef479a6b0f40eadcd5550e1007
Alataq <72706324+Alataq@users.noreply.github.com>
2022-10-19 16:54:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 const TOKEN = process.argv[2]
 console.log(TOKEN)
-const { REST, Routes } = require('discord.js');
-
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-];
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands("1032370103456641137"), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
+const commandsEmmiter = require("./commandsEmmiter.js")
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
