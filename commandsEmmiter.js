5a2aeee1b46e3d091f0a2530eb6470038053a2a7
Alataq <72706324+Alataq@users.noreply.github.com>
2022-10-19 16:56:01 -0400

    Create commandsEmmiter.js

---
+++ commandsEmmiter.js
@@ -0,0 +1,24 @@
+modules.export.emmit = (TOKEN) => {
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands("1032370103456641137"), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+}
